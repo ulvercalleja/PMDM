45c350bd2b49ec82ef94e09c22aca29b0fd46a74
Ulver <jculver@gmail.com>
2023-11-21 13:53:42 +0100

    Fragments

---
+++ app/src/main/java/pmdm/u2/ut04/u4a0FragmentDos.java
@@ -0,0 +1,66 @@
+package pmdm.u2.ut04;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import pmdm.u2.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link u4a0FragmentDos#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class u4a0FragmentDos extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public u4a0FragmentDos() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment u4a0FragmentDos.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static u4a0FragmentDos newInstance(String param1, String param2) {
+        u4a0FragmentDos fragment = new u4a0FragmentDos();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_u4a0_dos, container, false);
+    }
+}
